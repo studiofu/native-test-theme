9076b435888acff234f4f48d8565a39dde257dfa
Johnson Fu <johnson.fu@gmail.com>
2024-06-04 18:56:31 +0800

    custom pull to refresh

--- app/(tabs)/flatlistscreen.tsx
+++ app/(tabs)/flatlistscreen.tsx
@@ -1,6 +1,16 @@
-import { View, Text, FlatList, Pressable } from 'react-native'
+import { View, Text, FlatList, Pressable, PanResponder, RefreshControl } from 'react-native'
 import React, { Fragment, useEffect, useState } from 'react'
 import clsx from 'clsx';
+import Animated, { runOnJS, useAnimatedScrollHandler, useAnimatedStyle, useSharedValue, withDelay, withTiming } from 'react-native-reanimated';
+import { Gesture, GestureDetector } from 'react-native-gesture-handler';
+import { FontAwesome, MaterialCommunityIcons, MaterialIcons } from '@expo/vector-icons';
+
+
+// reference: 
+// https://blog.cloudboost.io/building-a-custom-refresh-animation-in-react-native-using-reanimated-9b64212a0366
+// https://medium.com/@taitasciore/handling-pan-and-scroll-gestures-simultaneously-and-gracefully-with-gesture-handler-2-reanimated-63f0d8f72d3c
+
+
 
 type PostItem = {
   userId: number;
@@ -13,19 +23,244 @@ const FlatListScreen = () => {
 
   const [data, setData] = useState<PostItem[] | []>([]);
   const [filterText, setFilterText] = useState<string>('');
+  const [refreshing, setRefreshing] = React.useState(false);
+  const [isPanEnabled, setIsPanEnabled] = useState(true);
+
+  const isReadyToRefresh = useSharedValue(false);
+  const scrollPosition = useSharedValue(0);
+  const pullDownPosition = useSharedValue(0);
+  const startScrollPosition = useSharedValue(0);
+  
+
+  // refresh function
+
+  const onRefresh = (done: () => void) => {
+    setRefreshing(true);
+    console.log('Refreshing...');
+  
+    setTimeout(() => {
+      console.log('Refreshed!');
+      setRefreshing(false);
+      done();
+    }, 3000);
+  };  
+
+
+  // ---------------------------------------------------------------------------
+  // use react native PanResponder
+
+  const onPanRelease = () => {
+    console.log('onPanRelease');
+    //pullDownPosition.value = withTiming(0, { duration: 180 });
+    pullDownPosition.value = withTiming(isReadyToRefresh.value ? 75 : 0, {
+      duration: 180,
+    });    
+
+    if (isReadyToRefresh.value) {
+      isReadyToRefresh.value = false;
+  
+      // Do Refresh
+
+      // A function that resets the animation
+      const onRefreshComplete = () => {
+        pullDownPosition.value = withTiming(0, { duration: 180 });
+      };
+
+      // trigger the refresh action
+      onRefresh(onRefreshComplete);
+
+    }    
+  };
+
+  const panResponderRef = React.useRef(
+    PanResponder.create({
+
+      // if scroll position is at the top and user is pulling down, i.e. dy > 0
+      onMoveShouldSetPanResponder: (event, gestureState) => {
+        return scrollPosition.value <= 0 && gestureState.dy >= 0;
+      },
+      
+      // set the dy to the pullDownPosition
+      onPanResponderMove: (event, gestureState) => {
+        const maxDistance = 150;        
+        pullDownPosition.value = Math.max(Math.min(maxDistance, gestureState.dy), 0);
+
+        // check if ready to refresh base on the pull down position
+        if (
+          pullDownPosition.value >= maxDistance / 2 &&
+          isReadyToRefresh.value === false
+        ) {
+          isReadyToRefresh.value = true;
+          console.log('Ready to refresh');
+        }
+  
+        if (
+          pullDownPosition.value < maxDistance / 2 &&
+          isReadyToRefresh.value === true
+        ) {
+          isReadyToRefresh.value = false;
+          console.log('Will not refresh on release');
+        }
+
+      },
+      onPanResponderRelease: onPanRelease,
+      onPanResponderTerminate: onPanRelease,
+    })
+  );
+
+  const pullDownStyles = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          translateY: pullDownPosition.value,
+        },
+      ],
+    };
+  });  
+
+
+
+  // assign to the height of the refresh container
+  const refreshContainerStyles = useAnimatedStyle(() => {
+    return {
+      height: pullDownPosition.value,
+    };
+  });
+
+  // assign to the refresh icon
+  const refreshIconStyles = useAnimatedStyle(() => {
+    const scale = Math.min(1, Math.max(0, pullDownPosition.value / 75));
+
+    return {
+      opacity: Math.max(0, pullDownPosition.value - 25) / 50,
+      //opacity: refreshing ? withDelay(100, withTiming(0, { duration: 200 })) : Math.max(0, pullDownPosition.value - 25) / 50,
+      transform: [
+        {
+          scale: scale,
+        },
+        {
+          rotate: `${pullDownPosition.value * 3}deg`,
+        },
+      ],
+    };
+  },[refreshing]);  
+  
+
+  // ---------------------------------------------------------------------------
+  // assign to the flatlist scroll event
+  // to get the scroll position
+  const scrollHandler = useAnimatedScrollHandler({
+    onScroll: (event) => {
+      scrollPosition.value = event.contentOffset.y;      
+    },  
+  });
+
+
+  
+  // gesture handling (not work)
+  const updatePanState = (offset: number) => {
+    console.log(`updatePanState: `,offset);
+    //'worklet';
+    if (offset > 0) {
+      //runOnJS(setIsPanEnabled)(false);
+    } else if (offset === 0) {
+      //runOnJS(setIsPanEnabled)(true);
+    }
+  };  
+
+  const onGestureRelease = () => {
+    console.log('onGestureRelease');
+    //pullDownPosition.value = withTiming(0, { duration: 180 });
+    pullDownPosition.value = withTiming(isReadyToRefresh.value ? 75 : 0, {
+      duration: 180,
+    });    
+
+    if (isReadyToRefresh.value) {
+      isReadyToRefresh.value = false;
+  
+      // Do Refresh
+
+      // A function that resets the animation
+      const onRefreshComplete = () => {
+        pullDownPosition.value = withTiming(0, { duration: 180 });
+      };
+
+      // trigger the refresh action
+      onRefresh(onRefreshComplete);
+
+    }    
+  };  
+
+  const scrollPanGesture = Gesture.Pan()
+  .onBegin(() => {
+    console.log(`gesture begin`);
+    startScrollPosition.value = scrollPosition.value;
+  })
+  .onUpdate((e) => {
+    //console.log(`gesture update`, e);
+    //console.log(`gesture update`, e.translationY);
+    //console.log(`gesture update scroll`, scrollPosition.value);
+
+    if(scrollPosition.value <= 0)  {
+
+      const maxDistance = 150;
+      //console.log('move ',gestureState.dy);
+      //pullDownPosition.value = gestureState.dy;
+      //pullDownPosition.value = Math.max(gestureState.dy, 0);
+      const deltaY = e.translationY - startScrollPosition.value;
+
+      // for animation to pull down
+      pullDownPosition.value = Math.max(Math.min(maxDistance, deltaY), 0);
+
+
+      // check if ready to refresh
+      if (
+        pullDownPosition.value >= maxDistance / 2 &&
+        isReadyToRefresh.value === false
+      ) {
+        isReadyToRefresh.value = true;
+        console.log('Ready to refresh');
+      }
+
+      if (
+        pullDownPosition.value < maxDistance / 2 &&
+        isReadyToRefresh.value === true
+      ) {
+        isReadyToRefresh.value = false;
+        console.log('Will not refresh on release');
+      }      
+    }
+  })
+  .onEnd(() => {
+    console.log(`gesture end`);
+  })
+  .onFinalize(() => {
+    console.log(`gesture finalize`);
+    //pullDownPosition.value = withTiming(0, { duration: 180 });
+
+    runOnJS(onGestureRelease)();
+  })  
+  ;  
+
+  const nativeGesture = Gesture.Native();
+
+  const composedGestures = Gesture.Simultaneous(
+    scrollPanGesture,
+    nativeGesture,
+  );  
+
+
+
+
+
+  // ---------------------------------------------------------------------------
+  // demo fetch data
 
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/posts')
     .then(response => response.json())
     .then(json => setData(json))
-  },[])
-
-
-  if(data.length <= 0) return (
-    <View><Text>Loading</Text></View>
-  )
-
-
+  },[])  
 
   const filteredData = data.filter((item, index) => {
     if(filterText == '') return true;  
@@ -39,6 +274,11 @@ const FlatListScreen = () => {
     return false;
   })
 
+  if(data.length <= 0) return (
+    <View><Text>Loading</Text></View>
+  )  
+
+  const AnimatedFontAwesomeIcon = Animated.createAnimatedComponent(FontAwesome);
 
   return (    
 
@@ -46,15 +286,68 @@ const FlatListScreen = () => {
 
       <View className='flex flex-row gap-2 p-2 m-2 items-center justify-center' > 
           <Pressable onPress={() => setFilterText('')}>
-            <Text className='bg-red-300 p-2 m-2 rounded-lg'>all</Text></Pressable>              
+            
+            <View className='bg-red-300 p-2 m-2 rounded-lg justify-center items-center flex flex-row'>
+              <MaterialCommunityIcons name="select-all" size={24} color="black" />
+              <Text>all</Text>
+            </View>
+            </Pressable>              
           <Pressable onPress={() => setFilterText('ion')}>
-            <Text className='bg-red-300 p-2 m-2 rounded-lg'>ion</Text></Pressable>
+          <View className='bg-red-300 p-2 m-2 rounded-lg justify-center items-center flex flex-row'>
+            <MaterialIcons name="extension" size={24} color="black" />
+            <Text>ion</Text>
+          </View>
+          </Pressable>
           <Pressable onPress={() => setFilterText('ia')}>
-            <Text className='bg-red-300 p-2 m-2 rounded-lg'>ia</Text></Pressable>
+          <View className='bg-red-300 p-2 m-2 rounded-lg justify-center items-center flex flex-row'>
+            <MaterialIcons name="mediation" size={24} color="black" />
+            <Text>ia</Text>
+          </View>  
+          </Pressable>
 
       </View>
-      <FlatList        
+      <View
+        style={{flex: 1, backgroundColor: '#f0f0f0'}}
+        //{...panResponderRef.current.panHandlers}
+      >
+      {/* <Animated.View style={[styles.refreshContainer, refreshContainerStyles]}>
+        <Animated.Image
+          source={refreshIcon}
+          style={[styles.refreshIcon, refreshIconStyles]}
+        />
+      </Animated.View>         */}
+
+      
+        <Animated.View style={[{          
+          width: '100%',          
+          justifyContent: 'center',
+          alignItems: 'center',   
+          position: 'absolute',     
+        }, refreshContainerStyles]}>
+        <AnimatedFontAwesomeIcon name="refresh" size={24} color="black"           
+          style={[refreshIconStyles]}
+        />
+        </Animated.View>
+
+      <GestureDetector gesture={composedGestures}>
+
+      <Animated.FlatList   
+        // refreshControl={
+        //   <RefreshControl
+        //     refreshing={false}
+        //     onRefresh={() => console.log('refreshing')}
+        //   />
+        // }
+
+        scrollEnabled={true}
+        style={[pullDownStyles]}
+        onScroll={scrollHandler}     
+        scrollEventThrottle={16} // Good practice for smooth performance
         data={filteredData}
+
+        bounces={false}
+        showsVerticalScrollIndicator={false}
+
         keyExtractor={(item) => item.id.toString()}
         stickyHeaderIndices={[0,10,20]}
         renderItem={({item, index}) => (
@@ -63,6 +356,10 @@ const FlatListScreen = () => {
           >{item.title}</Text>
         )}
       />
+      </GestureDetector>
+      </View>
+
+      
 
       </Fragment>
   )
--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,12 +1,197 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet, Button, Dimensions } from 'react-native'
 import React from 'react'
 
+import { Slider } from 'react-native-awesome-slider';
+import Swiper from 'react-native-swiper';
+import Animated, {
+  useAnimatedScrollHandler,
+  useScrollViewOffset,
+  useSharedValue,
+} from 'react-native-reanimated';
+import { RefreshControl, ScrollView } from 'react-native-gesture-handler';
+
+const data = [...new Array(6).keys()];
+const width = Dimensions.get("window").width;
+
 const Settings = () => {
+
+
+  const [refresh, setRefresh] = React.useState(false);
+
+  const pullToRefresh = () => {
+    console.log('refreshing.............')
+    setRefresh(true);
+    setTimeout(() => {
+      setRefresh(false);
+    }, 2000);
+  }
+
+  const progress = useSharedValue(30);
+  const min = useSharedValue(0);
+  const max = useSharedValue(100);
+
+
+  const scrollX = useSharedValue(0);
+  const onScrollHandler = useAnimatedScrollHandler((event) => {
+    scrollX.value = event.contentOffset.x;
+    console.log( `event.contentOffset.x: ${event.contentOffset.x}`)
+  });
+
+
+  const data = [
+    {
+      id: 1,
+      title: 'Modern JS: A curated collection'
+    },
+    {
+      id: 2,
+      title: 'JavaScript notes for professionals'
+    },
+    {
+      id: 3,
+      title: 'JavaScript: The Good Parts'
+    }
+  ]
+
+
+  
   return (
-    <View>
-      <Text>Settings</Text>
+    <View className='flex flex-col bg-red-50 h-full items-start' >
+      <Text className='p-5'>Settings</Text>
+
+      <Slider
+      //style={styles.container}
+      progress={progress}
+      minimumValue={min}
+      maximumValue={max}
+      thumbWidth={10}
+
+      theme={{
+        disableMinTrackTintColor: '#fff',
+        maximumTrackTintColor: '#fff',
+        minimumTrackTintColor: '#000',
+        cacheTrackTintColor: '#333',
+        bubbleBackgroundColor: '#666',
+        heartbeatColor: '#999',
+      }}      
+
+      onSlidingStart={() => {
+        console.log('start');
+      }}
+
+      onValueChange={(value) => {
+        console.log('change', value);
+      }}
+
+      
+
+    />
+
+    <View className='h-[200px] w-[90%] mt-10 self-center'>
+
+      <Swiper style={styles.wrapper} 
+        showsButtons={false}                
+        >
+        <View style={styles.slide1}>
+          <Text style={styles.text}>Hello Swiper</Text>
+          <Button             
+            onPress={() => {}}
+            title="Go to Home" />
+        </View>
+        <View style={styles.slide2}>
+          <Text style={styles.text}>Beautiful</Text>
+        </View>
+        <View style={styles.slide3}>
+          <Text style={styles.text}>And simple</Text>
+        </View>
+      </Swiper>
+      
+
+
+
+
+      </View>
+
+
+      {/** use animated flatlist */}
+      <View>
+        <ScrollView style={{flex: 1}}
+          refreshControl={
+            <RefreshControl
+              refreshing={refresh}
+              onRefresh={pullToRefresh}
+            />
+          }
+        >
+        <Animated.FlatList 
+          // refreshing={true}
+          // onRefresh={() => {
+          //   console.log('refreshing');
+          // }}
+          //horizontal
+          onScroll={onScrollHandler}
+          //pagingEnabled={true}
+          data={data}
+          scrollEnabled={false}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={({ item }) => (
+            <View className='h-[100px] w-[300px] bg-slate-300'>
+            <Text >
+              {item.title}
+              
+            </Text>
+            </View>
+          )}       
+          
+          ListHeaderComponent={() => (
+            <View className='h-[100px] w-[300px] bg-slate-300'>
+              <Text>Header</Text>
+            </View>
+          )}
+
+          ListFooterComponent={() => (
+            <View className='h-[500px] w-[300px] bg-slate-300'>
+              <Text>Footer</Text>
+            </View>
+          )}
+        />
+        </ScrollView>
+                  
+
+      </View>
+
+    
     </View>
   )
 }
+
+
+
+const styles = StyleSheet.create({
+  wrapper: {},
+  slide1: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#9DD6EB'
+  },
+  slide2: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#97CAE5'
+  },
+  slide3: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#92BBD9'
+  },
+  text: {
+    color: '#fff',
+    fontSize: 30,
+    fontWeight: 'bold'
+  }
+})
 
 export default Settings
